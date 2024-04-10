f2612a003c188bd11dd6d981700aaa2b3fe45fab
qinghu1 <qingqinghu279@gmail.com>
2024-04-10 18:09:04 -0400

    Wrote initial header code and set up the files for our pages

---
+++ my-app/src/Header.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+
+export default function Header() {
+return (
+<div className="header_container">
+<div className="header_left">
+</div>
+<div className="header_middle">
+    <Link className="link_middle" to="/">Bias Awareness</Link>
+</div>
+<div className="header_right">
+<Link className="link" to="/Workplace">Workplace</Link>
+<Link className="link" to="/Classroom">Classroom</Link>
+<Link className="link" to="/SocialLife">Social Life</Link>
+<Link className="link" to="/Resources">Resources</Link>
+</div>
+</div>
+);
+}
