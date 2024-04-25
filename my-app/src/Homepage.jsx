de9a8b26e860f3a799cd254b30545a977cd031ac
camtaylor2 <camtaylor245@gmail.com>
2024-04-24 22:01:51 -0400

    Increased font size on home page

--- my-app/src/Homepage.jsx
+++ my-app/src/Homepage.jsx
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom';
 function Homepage() {
   return (
     <div className="Homepage">
+
+
       <div className="ImageContainer">
-        <img src="https://static.scientificamerican.com/sciam/cache/file/89FF8E81-DB68-4EDC-95B6045E9E6EDA51_source.jpg?w=1000" alt="Unconscious Biases" className="MainImage" />
-      </div>
-      <div className="TextContainer">
-        <div className="BlueTextRight">
+       <div className="BlueTextAbove">
           <p>
             Unconscious biases are social stereotypes about certain groups of people that individuals form outside their own conscious awareness. Everyone holds unconscious beliefs about various social and identity groups.
           </p>
+        <img src="https://static.scientificamerican.com/sciam/cache/file/89FF8E81-DB68-4EDC-95B6045E9E6EDA51_source.jpg?w=1000" alt="Unconscious Biases" className="MainImage" />
+      </div>
+      <div className="TextContainer">
         </div>
         <div className="BlueTextBelow">
           <p>
